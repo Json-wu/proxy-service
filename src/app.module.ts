cce7d0a16e311fe4a51d99f84aa0d2bdc72dc9e0
jason <wyqctt@gmail.com>
2025-08-06 17:53:09 +0800

    edit env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { LogModule } from './log/log.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/proxy-logs'), // MongoDB 连接字符串
+    MongooseModule.forRoot(process.env.MONGODB_URI_PROXY || 'mongodb://localhost:27017/proxy-logs'), // MongoDB 连接字符串
     LogModule,
   ],
   controllers: [AppController],
